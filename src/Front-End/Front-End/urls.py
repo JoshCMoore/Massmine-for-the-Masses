c4dab1e828b1800b8daece9d2c1cfb0a3511cfc5
Josh <joshmoore431@gmail.com>
2019-02-06 17:00:25 -0500

    Create user page now functioning. I have yet to try to connect this to Patti's user database but that is my next step.

--- src/Front-End/Front-End/urls.py
+++ src/Front-End/Front-End/urls.py
@@ -19,6 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^accounts/', include('accounts.urls')),
 	url(r'^accounts/', include('django.contrib.auth.urls')),
 	url('', TemplateView.as_view(template_name='home.html'),name='home')
 ]
