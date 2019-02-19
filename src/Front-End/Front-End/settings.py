f8bde6620619133f7f1fe0b577c20505d31d4f77
PatriciaTanzer <ptanzer8@gmail.com>
2019-02-19 13:25:04 -0800

    Fixed user error by removing user

--- src/Front-End/Front-End/settings.py
+++ src/Front-End/Front-End/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'users.apps.UsersConfig',
     ]
 
 MIDDLEWARE = [
@@ -124,5 +123,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'home'
 
-# Authentication needs to use custom user
-AUTH_USER_MODEL = 'users.CustomUser'
+
