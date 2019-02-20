1e4ed37d2c0b109067d5215ed437e719fd81c371
PatriciaTanzer <ptanzer8@gmail.com>
2019-02-20 11:32:16 -0800

    Updgraded project to Django 2.1

--- src/Front-End/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-	url(r'^signup/',views.SignUp.as_view(),name='signup'),
-]
---
+++ src/webapp/accounts/urls.py
@@ -0,0 +1,7 @@
+# acccounts/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('signup/', views.SignUp.as_view(), name='signup'),
+        ]
