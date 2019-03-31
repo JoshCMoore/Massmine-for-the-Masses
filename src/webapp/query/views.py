5b52ce3752be52323d575d1e010cbc1786c0381f
loganhornbuckle <lbhornbu@uncg.edu>
2019-03-31 15:19:03 -0400

    updated query page

--- src/webapp/query/views.py
+++ src/webapp/query/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render
 from django.core.management import call_command
 from django.http import HttpResponse
 from django.urls import reverse
+from query.forms import QueryForm
 from query.models import Tweet
+from subprocess import Popen, PIPE
 import pandas as pd
 import numpy as np
 import datetime
 import subprocess
 import json
-import os
+import ast
 
 def index(request):
 	return render(request, 'index.html')
@@ -20,30 +22,88 @@ def request_page(request):
 
 def make_query(request):
 	
-	keyword = request.POST.get("keyword")
-	count = request.POST.get("count")
-	
-	str1 = "massmine --task=twitter-search --count="
-	str2 = " --query="
-	str3 = " | jsan --output="
-	str4 = ".csv"
+	keyword = request.POST.get('keyword')
+	count = request.POST.get('count')
 
-	command = str1 + count + str2 + "\""+keyword+"\"" + str3 + "\""+keyword+"\"" + str4
+	command = 'massmine --task=twitter-search --count=' + count + ' --query=' + keyword
 
-	os.system(command)
+	stdout = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
 
-	df = pd.read_csv('/home/parallels/django/webapp/'+keyword+'.csv')
+	output = stdout.readlines()
 
-	for row in df.iterrows():
-	    for x in range(1,len(row)):
-	    	#need code here to insert into DB
-	        #created_at = row[x]['created_at']
-	        tweet_id_str = row[x]['id_str']
-	        #print(tweet_id_str)
-	        text = row[x]['text']
-	        tweet_instance = Tweet.objects.create()
-	        tweet_instance.save(tweet_id_str)
+	for i in output:
+		string = i.decode("utf-8")
+		data = json.loads(string)
 
-	return HttpResponse("Success!")
+		try:
 
-	# massmine --task=twitter-stream --query=love --count=200 | jsan --output=mydata.csv
+			for key,value in data.items():
+				if (key == 'id_str'):
+					tid = value
+				if (key == 'created_at'):
+					ca = value
+				if (key == 'text'):
+					txt = value
+				if (key == 'source'):
+					src = value
+				if (key == 'truncated'):
+					trunc = value
+				if (key == 'retweet_count'):
+					re_count = value
+				if (key == 'metadata'):
+					for key,value in data['metadata'].items():
+						if (key == 'iso_language_code'):
+							language = value
+				if (key == 'user'):
+					for key,value in data['user'].items():
+						if (key == 'id_str'):
+							uid = value
+						if (key == 'location'):
+							cntry = value
+						if (key == 'name'):
+							nme = value
+						if (key == 'screen_name'):
+							scr_name= value
+						if (key == 'url'):
+							u = value
+						if (key == 'description'):
+							desc = value
+						if (key == 'verified'):
+							verify = value
+						if (key == 'followers_count'):
+							fol_count = value
+						if (key == 'listed_count'):
+							list_count = value
+						if (key == 'favourites_count'):
+							fav_count = value
+						if (key == 'statuses_count'):
+							tw_count = value
+						if (key == 'utc_offset'):
+							utc_off = value
+						if (key == 'friends_count'):
+							fr_count = value
+						if (key == 'time_zone'):
+							tz = value
+						if (key == 'geo_enabled'):
+							geo_en = value
+				if (key == 'in_reply_to_status_id_str'):
+					reply_sid = value
+				if (key == 'in_reply_to_user_id_str'):
+					reply_uid = value
+				if (key == 'in_reply_to_screen_name'):
+					reply_scrname = value
+
+			tweet = Tweet(tweet_id_str=tid,created_at=ca,text=txt,device=src,truncated=trunc,
+				retweet_count=re_count,lang=language,country=cntry,user_id_str=uid,name=nme,
+				screen_name=scr_name,in_reply_to_status_id_str=reply_sid,
+				in_reply_to_user_id_str=reply_uid,in_reply_to_screen_name=reply_scrname,url=u,description=desc,verified=verify,
+				followers_count = fol_count,friends_count=fr_count,listed_count=list_count,favourites_count=fav_count,
+				num_tweets=tw_count,utc_offset=utc_off,time_zone=tz,geo_enabled=geo_en)
+
+			tweet.save()
+
+		except:
+			print("ERROR")
+		
+	return HttpResponse(output)
+
